7821964ff86a71a5d41ac59165c3edfb9299c937
umairrazam <umairrazam@gmail.com>
2021-03-16 16:42:30 +0500

    fix(best-flavor): according to theme

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,12 +1,14 @@
 Spree::ProductsController.class_eval do
 
-  before_action :load_best_sellers, only: [:index, :show]
+  	before_action :load_best_sellers, only: [:index, :show, :best_seller]
 
+  	helper 'spree/taxons'
 
-
-
-
-
-
+	def best_seller
+		@taxonomies = Spree::Taxonomy.accessible_by(current_ability).order('name').includes(root: :children).
+                        ransack(params[:q]).result.
+                        page(params[:page]).per(params[:per_page])
+		@products = @best_sellers
+	end
 
 end
