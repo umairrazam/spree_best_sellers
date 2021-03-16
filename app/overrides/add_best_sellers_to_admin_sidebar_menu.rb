7277945b01fb763b15b3c76fa1ac8f989bf6f54c
umairrazam <umairrazam@gmail.com>
2021-03-16 16:54:33 +0500

    fix(design): remove spree designs and add theme designs

--- app/overrides/add_best_sellers_to_admin_sidebar_menu.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
-  name: 'best_sellers_admin_sidebar_menu',
-  insert_bottom: '#main-sidebar',
-  partial: 'spree/admin/shared/best_sellers_sidebar_menu'
-)
